cbf991197baff91d024093f92048cfc42c987954
Eric Brian Njuguna <eric.njuguna@student.moringaschool.com>
2024-07-15 20:21:14 +0300

    Adds DOM modifying elementsin index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+
+// Remove the <main> with id 'main'
+const main = document.getElementById("main");
+main.remove();
+
+// Created an h1 element and assigned it to the variable newHeader then later given it an id of victory
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+// Added Yobra is the champion as the content of the our h1 element stored in the newHeader variable
+newHeader.innerHTML = "Yobra is the champion";
